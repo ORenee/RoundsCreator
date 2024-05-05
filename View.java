1a3f8f33a5d081770d663fcd8b76e885ae7b2322
Renee Olds <olds0029@d.umn.edu>
2024-05-05 17:34:19 -0500

    Moved creation code out of main, started creating UI elements

---
+++ View.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class View {
+    private JTextArea title;
+    private JList resultList;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
